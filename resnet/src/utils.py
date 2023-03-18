af1899b3893fa09f4f265e27abda1a23f39a4ddc
Hongxuan Liu <fengjianche@pinduoduo.com>
2023-03-17 18:56:45 -0600

    fixed bug

--- resnet/src/utils.py
+++ resnet/src/utils.py
@@ -7,13 +7,13 @@ def load_config(path=None):
     if path is None:
         path = os.path.join(
         pathlib.Path(__file__).parent.resolve(),
-        '../configs/config.yaml')
+        'resnet/configs/config.yaml')
         
     with open(path, 'r') as f:
         return yaml.load(f, Loader=yaml.FullLoader)
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--config', type=str, default='../configs/config.yaml')
+    parser.add_argument('--config', type=str, default='resnet/configs/config.yaml')
     args = parser.parse_args()
     return args
